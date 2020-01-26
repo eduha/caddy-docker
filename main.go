bd31de31c8662aa25739b6ff9910b0c0b7778b10
eduha <admin@eduha.info>
2020-01-27 00:16:50 +0600

    Update main.go

--- main.go
+++ main.go
@@ -30,7 +30,7 @@ import (
 	caddycmd "github.com/caddyserver/caddy/v2/cmd"
 
 	// plug in Caddy modules here
-	//_ "github.com/caddyserver/dnsproviders/rfc2136"
+	_ "github.com/caddyserver/dnsproviders/rfc2136"
   
 	_ "github.com/caddyserver/caddy/v2/modules/standard"
 )
